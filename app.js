df1ee29b5859cc0b355ceef73907e8238ec28694
jjbarros.e <jjbarros.e@gmail.com>
2024-10-25 23:20:55 -0500

    se agrega sesion

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 const port = 3000
 // Get the client
 const mysql = require('mysql2/promise');
 const cors = require('cors')
+const session = require('express-session')
 
-app.use(cors())
+app.use(cors(
+  {
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }
+))
+app.use(session({
+  secret: 'nklslbskflbs34f78sfbksjhrdn346fvkbsd'
+}))
 
 // Create the connection to database
 const connection = mysql.createPool({
@@ -19,6 +28,7 @@ async function login(req, res) { //req peticion, res respuesta
   const [filas] = await connection.query("SELECT * FROM  `usuarios` WHERE `usuario` = '"+datos.usuario+"' AND `contraseña` = '"+datos.clave+"'")
 
   if (filas.length ==1) {
+    req.session.usuario = datos.usuario
     res.status(200).json({ logueado: true })
   } else {
     res.status(401).json({ error: 'Usuario o contraseña incorrecta'})
@@ -26,6 +36,21 @@ async function login(req, res) { //req peticion, res respuesta
 }
 
 app.get('/login', login)
+
+function validar(req, res) {
+  if (req.session.usuario) {
+    res.status(200).json({ logueado: true})
+  } else{
+    res.status(401).json({ error: 'Usuario no logueado'})
+  }
+}
+
+app.get('/validar', validar)
+
+app.get('/cerrar', (req, res) => {
+  req.session.destroy()
+  res.status(200).json({ logueado: false})
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
